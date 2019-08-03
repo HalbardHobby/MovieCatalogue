9aeff3316fe92d268e5eb1b3a062718c24654c4c
Alejandro Espinosa <HalbardHobby@users.noreply.github.com>
2019-08-03 11:04:34 -0500

    added catalogue app

--- moviecatalogue/urls.py
+++ moviecatalogue/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    
+    # Handle all non api urls through react frontend
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
