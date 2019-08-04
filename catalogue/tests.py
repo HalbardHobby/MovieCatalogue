78af62727aec00c64132b99ed00be5e66d7b8f4d
Alejandro Espinosa <HalbardHobby@users.noreply.github.com>
2019-08-04 12:08:06 -0500

    Added Tests

--- catalogue/tests.py
+++ catalogue/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from catalogue.models import Genre, Movie
 
-# Create your tests here.
+class MovieTestCase(TestCase):
+    def setUp(self):
+        self.genre = Genre.objects.create(name='Comic')
+
+
+    def test_runtime_validation(self):
+        with self.assertRaises(ValidationError):
+            m = Movie.objects.create(primary_title='Iron Man',
+                                     original_title='Iron Man',
+                                     year=2008,
+                                     runtime_minutes=-15
+                                     )
+            m.full_clean()
+
+
+    def test_year_validation(self):
+        with self.assertRaises(ValidationError):
+            m = Movie.objects.create(primary_title='Iron Man',
+                                     original_title='Iron Man',
+                                     year=1785,
+                                     runtime_minutes=124
+                                     )
+            m.full_clean()
+
+        with self.assertRaises(ValidationError):
+            m = Movie.objects.create(primary_title='Iron Man',
+                                     original_title='Iron Man',
+                                     year=2950,
+                                     runtime_minutes=124
+                                     )
+            m.full_clean()
